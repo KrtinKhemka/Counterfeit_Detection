04f8801f692002f38315a1e2753479d1da5c961a
Snimso <115739027+Snimso@users.noreply.github.com>
2024-06-20 16:20:13 +0530

    myenv

--- roberta.py
+++ roberta.py
@@ -1,9 +1,41 @@
 from transformers import AutoTokenizer
 from transformers import AutoModelForSequenceClassification
 from scipy.special import softmax
-
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 MODEL = f"cardiffnlp/twitter-roberta-base-sentiment"
 tokenizer = AutoTokenizer.from_pretrained(MODEL)
 model = AutoModelForSequenceClassification.from_pretrained(MODEL)
 
+dfmain = pd.read_csv('final_dataset.csv')
 
+def polarity_scores_roberta(example):
+    encoded_text = tokenizer(example, return_tensors='pt')
+    output = model(**encoded_text)
+    scores = output[0][0].detach().numpy()
+    scores = softmax(scores)
+    scores_dict = {
+        'roberta_neg' : scores[0],
+        'roberta_neu' : scores[1],
+        'roberta_pos' : scores[2]
+    }
+    return scores_dict
+
+res = {}
+Id=[]
+for i in range(len(dfmain)):
+    text = dfmain['review'].iloc[i]
+    id = i
+    Id.append(id)
+    res[id] = sia.polarity_scores(text)
+
+dfmain['Id'] = range(len(dfmain))
+vaders_result = pd.DataFrame(res).T
+vaders_result['Id'] = range(len(dfmain))
+vaders_result = vaders_result.merge(dfmain, how='left')
+#print(vaders_result['compound'])
+ax = sns.barplot(data = vaders_result, x = 'ratings', y='compound')
+ax.set_title('vader plot')
+plt.show()
